4a3025d414eafb31fe202c9f39450efb4a7abbfe
Jagdish <kjagdish696.jk@gmail.com>
2026-04-27 23:21:23 +0530

    fixed vercel issue

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 import { firebaseAuth } from "./firebase";
 
-const BASE_URL = "http://127.0.0.1:8001";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8001";
 
 export const apiClient = axios.create({ baseURL: BASE_URL });
 
